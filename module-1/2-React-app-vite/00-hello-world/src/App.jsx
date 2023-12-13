dfe7bf57c60c87cde20467bfeef0cc0bec308079
Richard Gutierrez Condori <rychard.gutierrez@gmail.com>
2023-12-12 22:31:32 -0500

    Use Arrays

--- module-1/2-React-app-vite/00-hello-world/src/App.jsx
+++ module-1/2-React-app-vite/00-hello-world/src/App.jsx
@@ -3,29 +3,40 @@ import TwitterFollowCard from './components/TwitterFollowCard';
 
 import './App.css';
 
+const users = [
+  {
+    userName: 'Fox',
+    isFollowing: false,
+    name: 'Fox the user',
+  },
+
+  {
+    userName: 'Lucas',
+    isFollowing: true,
+    name: 'Lucas the user',
+  },
+
+  {
+    userName: 'Ana',
+    isFollowing: false,
+    name: 'Ana the user',
+  },
+];
+
 function App() {
   const formatterUserName = (userName) => <span>@ {userName}</span>;
   return (
     <div className="twitter-container">
-      <TwitterFollowCard
-        imgUrl="https://media.npr.org/assets/img/2014/01/02/140102123909_1_wide-da99ab742e0a327bf840d4b8e01b864b019482ca-s1400-c100.jpg"
-        initialIsFollowing={true}
-        formattedUserName={formatterUserName('foxy')}
-      >
-        Fox the user
-      </TwitterFollowCard>
-      <TwitterFollowCard
-        imgUrl="https://bellzi.com/cdn/shop/products/Lr-9890-sw.jpg?v=1658216634"
-        initialIsFollowing={false}
-        name="Fox"
-        formattedUserName={formatterUserName('foxy')}
-      />
-
-      <TwitterFollowCard
-        imgUrl=""
-        initialIsFollowing={false}
-        formattedUserName={formatterUserName('Unknown')}
-      />
+      {users.map(({ userName, isFollowing, name }) => (
+        <TwitterFollowCard
+          imgUrl="https://media.npr.org/assets/img/2014/01/02/140102123909_1_wide-da99ab742e0a327bf840d4b8e01b864b019482ca-s1400-c100.jpg"
+          initialIsFollowing={isFollowing}
+          formattedUserName={formatterUserName(userName)}
+          key={userName}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
     </div>
   );
 }
